7098ff252052fd573ab85c6c22214a6914f4fe06
Stewfiin <114639683+stewfiin@users.noreply.github.com>
2025-10-24 23:38:21 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CvController;
+use App\Http\Controllers\cvController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
